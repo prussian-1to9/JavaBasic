72d1acd9d4899fbd23eb620a60966e148d7e58c6
prussian-1to9 <prussian.3to3@gmail.com>
2023-11-13 21:27:53 +0900

    refactoring : basic syntaxes

---
+++ start/src/i_basic_syntax/Comment.java
@@ -0,0 +1,28 @@
+package i_basic_syntax;
+
+/**
+ * 주석의 형식을 보여주기 위한 클래스입니다.
+ * 
+ * 해당 주석은 배포 문서에 포함되는 주석입니다.
+ * @author prussian.3to3@gmail.com
+ * 
+ * 위와 같이 annotation도 사용 가능합니다.
+ */
+
+public class Comment {
+	// 한 줄 주석
+
+	// 진입점 함수
+	public static void main(String[] args) {
+	
+	// 한 줄 주석 작성
+	System.out.println("한 줄 주석 작성 완료");
+	// 한 줄 주석 재작성 System.out.println();
+	// 주석 안의 구문은 콘솔창에 print 되지 않는 것이 확인 가능합니다.
+	
+	/*
+		여러 줄 주석
+		이 안에서 작성되는 내용은 모두 주석으로 처리됩니다.
+	 */
+	}
+}
---
+++ start/src/i_basic_syntax/README.md
@@ -0,0 +1,14 @@
+# Baisc Syntax : java 기본 구문
+- Comment.java : 주석 작성법
+    - 한 줄 주석 `// `
+    - 여러 줄 주석 `/* */`
+    - 배포 주석 `/** */`
+
+***
+# 개념
+## 클래스 블록(Class block)
+클래스 블록 바로 하단에는 **일반 명령**이 올 수 없고, 아래의 class member만 올 수 있습니다.  
+**클래스는 변수와 함수로 구성된다**고 이해하면 쉽겠습니다.
+- field : 변수(member variable)
+- 함수 : 클래스 메소드(class method), 생성자(constructor)
+static 맴버 선언 시 별도의 **객체 생성 없이**도 **명령문 작성이 가능**합니다.
